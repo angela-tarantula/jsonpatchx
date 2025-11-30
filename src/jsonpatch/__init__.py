cc4f9a0367ad2ca5936a13f2aad86e06ee965a39
Angela Liss <59097311+angela-tarantula@users.noreply.github.com>
2025-11-30 10:31:48 -0500

    proper structure in src

---
+++ src/jsonpatch/__init__.py
@@ -0,0 +1,17 @@
+from jsonpatch.exceptions import (
+    MissingMember,
+    UnrecognizedOperation,
+    MemberTypeMismatch,
+    MemberValueMismatch,
+    JsonPatchConflict,
+    JsonPatchTestFailed,
+)
+
+__all__ = [
+    "MissingMember",
+    "UnrecognizedOperation",
+    "MemberTypeMismatch",
+    "MemberValueMismatch",
+    "JsonPatchConflict",
+    "JsonPatchTestFailed",
+]
