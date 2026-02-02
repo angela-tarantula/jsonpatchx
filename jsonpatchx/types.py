88ac65bde6d2d425a5ee0d6741cfead131e2a10d
Angela Liss <59097311+angela-tarantula@users.noreply.github.com>
2026-02-01 23:44:44 -0500

    support Py3.12+ and improve CI
    * testing coverage CI
    
    * show covered
    
    * skip covered
    
    * show covered
    
    * test 3.13 too
    
    * 3.12 too
    
    * py3.12+
    
    * defer annotations, fail fast false
    
    * 3.13+ instead
    
    * dont use typeis yet
    
    * 3.14+ comment
    
    * try 3.12 again
    
    * use TypeIs from typing_extensions instead!
    
    * add 3.14 note
    
    * skip covered
    
    * code coverage in readme

--- jsonpatchx/types.py
+++ jsonpatchx/types.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
 from functools import lru_cache
-from typing import Annotated, TypeIs
+from typing import Annotated
 
 from pydantic import Field, TypeAdapter
-from typing_extensions import TypeForm
+from typing_extensions import TypeForm, TypeIs
 
 from jsonpatchx.exceptions import InvalidJSONPointer
 
