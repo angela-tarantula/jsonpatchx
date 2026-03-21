41b561493ff28b026f5d3ec07bf985b0a03947e1
Angela Liss <59097311+angela-tarantula@users.noreply.github.com>
2026-03-20 20:42:15 -0400

    dockerfile

---
+++ .clusterfuzzlite/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -eu
+
+# Install harness dependency + this package into the builder image.
+python -m pip install --upgrade pip
+python -m pip install atheris
+python -m pip install .
+
+# Compile fuzz targets into libFuzzer-compatible entrypoints
+compile_python_fuzzer fuzzers/jsonpatchx_fuzzer.py \
+  --output_name jsonpatchx_fuzzer
+compile_python_fuzzer fuzzers/jsonpatchx_custom_backend_fuzzer.py \
+  --output_name jsonpatchx_custom_backend_fuzzer
+
+# Ship dictionaries for token-aware mutations
+cp fuzzers/jsonpatchx_fuzzer.dict "$OUT/jsonpatchx_fuzzer.dict"
+cp fuzzers/jsonpatchx_custom_backend_fuzzer.dict \
+  "$OUT/jsonpatchx_custom_backend_fuzzer.dict"
+
+# Ship seed corpora for both targets
+zip -j "$OUT/jsonpatchx_fuzzer_seed_corpus.zip" \
+  fuzzers/corpus/jsonpatchx_fuzzer/*
+zip -j "$OUT/jsonpatchx_custom_backend_fuzzer_seed_corpus.zip" \
+  fuzzers/corpus/jsonpatchx_custom_backend_fuzzer/*
