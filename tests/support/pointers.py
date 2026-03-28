bbf8d15ed857adde2dca3c07177a3697d04dd46d
Angela Liss <59097311+angela-tarantula@users.noreply.github.com>
2026-03-28 13:51:52 -0400

    unused method

--- tests/support/pointers.py
+++ tests/support/pointers.py
@@ -24,14 +24,6 @@ class IncompletePointerBackend:
     def __str__(self) -> str:
         return ".".join(self._parts)
 
-    def __hash__(self) -> int:
-        return hash(tuple(self._parts))
-
-    def __eq__(self, other: object) -> bool:
-        if not isinstance(other, IncompletePointerBackend):
-            return NotImplemented
-        return self._parts == other._parts
-
 
 class AnotherIncompletePointerBackend(IncompletePointerBackend, PointerBackend):
     """IncompletePointerBackend but it technically inherits from PointerBackend."""
